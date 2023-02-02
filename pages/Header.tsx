30b05148c7f8570f57fc9f13092aebf37ab8ae5e
harsh-kumar-saaniya <harshvithal1@gmail.com>
2023-02-03 02:59:05 +0500

    project completed and ready to push on vercel to deploy the project

---
+++ pages/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='bg-violet-800 text-white text-center pt-20 pb-52'>
+            <div>
+                <h1 className='text-4xl font-bold' >Simple pricing by Harish Vithal</h1>
+            </div>
+            <div className='mt-3'>
+                <p>Plans that are carefully crafted to suit your business</p>
+            </div>
+        </div>
+    )
+}
